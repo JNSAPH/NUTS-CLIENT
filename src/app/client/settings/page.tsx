ea611c3437e37344f86b53eeee7b6e922dc196c5
Jonas <JNSAPH@icloud.com>
2025-01-13 22:29:37 +0100

    Version Bump

--- src/app/client/settings/page.tsx
+++ src/app/client/settings/page.tsx
@@ -6,6 +6,8 @@ import { setClientSettings } from "@/redux/slices/windowProperties";
 import { clearPersistedData, RootState } from "@/redux/store";
 import { useDispatch, useSelector } from "react-redux";
 
+const packageJSON = require("../../../../package.json");
+
 export default function Page() {
   const content = useSelector((state: RootState) => state.windowProperties);
   const dispatch = useDispatch();
@@ -13,6 +15,8 @@ export default function Page() {
   return (
     <div className="p-4 h-full w-full space-y-2">
       <p className="text-3xl font-bold">Settings</p>
+      <p>Nuts Version {packageJSON.version}</p>
+      <hr className="border border-clientColors-card-border"/>
       <div className="space-y-4">
       <div className="flex items-center space-x-2">
         <Switch id="airplane-mode" onCheckedChange={(state) => {
